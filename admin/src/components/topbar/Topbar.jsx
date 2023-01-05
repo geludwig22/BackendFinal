c879b9439d316f3c7f8bf240457510594b3ef553
geludwig22 <jorlupeve@hotmail.com>
2023-01-04 23:23:01 -0300

    first commit

---
+++ admin/src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="https://thumbs.dreamstime.com/b/inicio-de-sesi%C3%B3n-administrador-en-el-icono-del-port%C3%A1til-vector-stock-166205404.jpg" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ admin/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0kgmSi0pSUT-W01EbHAC42AM7vrbp9K4",
+  authDomain: "proyectofinal-1fa5f.firebaseapp.com",
+  projectId: "proyectofinal-1fa5f",
+  storageBucket: "proyectofinal-1fa5f.appspot.com",
+  messagingSenderId: "941750697617",
+  appId: "1:941750697617:web:42a70b63c76e903f638930"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
